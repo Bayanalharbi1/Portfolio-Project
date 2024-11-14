86de887c845637150e94edf01e34669b43a59ad5
Bayanalharbi1 <bayanlharbi@gmail.com>
2024-11-14 14:02:45 +0300

    Fixed skills.HTML page .

--- personal_website/portfolio/urls.py
+++ personal_website/portfolio/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.project_list, name='project_list'),
-path('skills/', views.skills_view, name='skills_view'),]
+path('projects/', views.project_list, name='project_list'),
+path('skills/', views.skills_view, name='skills_view'),
+]
