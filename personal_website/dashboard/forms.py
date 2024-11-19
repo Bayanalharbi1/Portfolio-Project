f6566d392dfed9be484e9f44fd515791453be9e7
Bayanalharbi1 <bayanlharbi@gmail.com>
2024-11-19 16:09:04 +0300

    add my resume as image

--- personal_website/dashboard/forms.py
+++ personal_website/dashboard/forms.py
@@ -4,9 +4,9 @@ from .models import Skill, Project
 class SkillForm(forms.ModelForm):
     class Meta:
         model = Skill
-        fields = ['name', 'proficiency']
+        fields = ['name', 'proficiency', 'image']
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'description', 'date_completed']
+        fields = ['title', 'description', 'date_completed', 'image']
--- personal_website/dashboard/models.py
+++ personal_website/dashboard/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
-    proficiency = models.CharField(max_length=50)  
+    proficiency = models.IntegerField()
+    image = models.ImageField(upload_to='skills/', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -11,6 +12,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_completed = models.DateField()
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
 
     def __str__(self):
         return self.title
